5a2f438cad75ec40e67f7e181a3fbec32f5a8c48
zezosarg <cs04588@uoi.gr>
2023-05-21 00:30:42 +0300

    chart form improvement

--- src/main/java/com/mye030/world_data_visualizer/model/FormData.java
+++ src/main/java/com/mye030/world_data_visualizer/model/FormData.java
@@ -6,14 +6,20 @@ public class FormData {
 	private String chartType;
 	private List<String> countries;
 	private List<String> indicators;
-	
+	private int minYear;
+	private int maxYear;
+	private int aggregate;
+
 	public FormData() {}
 	
-	public FormData(String chartType, List<String> countries, List<String> indicators) {
+	public FormData(String chartType, List<String> countries, List<String> indicators, int maxYear, int minYear, int aggregate) {
 		super();
 		this.chartType = chartType;
 		this.countries = countries;
 		this.indicators = indicators;
+		this.minYear = minYear;
+		this.maxYear = maxYear;
+		this.aggregate = aggregate;
 	}
 
 	public String getChartType() {
@@ -40,10 +46,34 @@ public class FormData {
 		this.indicators = indicators;
 	}
 
+	public int getMinYear() {
+		return minYear;
+	}
+
+	public void setMinYear(int minYear) {
+		this.minYear = minYear;
+	}
+
+	public int getMaxYear() {
+		return maxYear;
+	}
+
+	public void setMaxYear(int maxYear) {
+		this.maxYear = maxYear;
+	}
+
+	public int isAggregate() {
+		return aggregate;
+	}
+
+	public void setAggregate(int aggregate) {
+		this.aggregate = aggregate;
+	}
+
 	@Override
 	public String toString() {
-		return "FormData [chartType=" + chartType + ", countries=" + countries + ", indicators=" + indicators + "]";
+		return "FormData [chartType=" + chartType + ", countries=" + countries + ", indicators=" + indicators
+				+ ", minYear=" + minYear + ", maxYear=" + maxYear + ", aggregate=" + aggregate + "]";
 	}
-	
 	
 }
--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -1,91 +1,104 @@
 $(document).ready(function() {
 			
-	$('.addPair').click(function() {
-		
-		var indicatorOptionsHtml = '<option value="" selected hidden>Indicator</option>'; 
-		for (let indicator of indicatorsList)
-			indicatorOptionsHtml += '<option value="'+indicator+'">'+indicator+'</option>';
+			$('.addPair').click(function() {
 				
-		var countryOptionsHtml = '<option value="" selected hidden>Country</option>';
-		for (let country of countriesList)
-			countryOptionsHtml += '<option value="'+country+'">'+country+'</option>';
+				var indicatorOptionsHtml = '<option value="" selected hidden>Indicator</option>'; 
+				for (let indicator of indicatorsList)
+					indicatorOptionsHtml += '<option value="'+indicator+'">'+indicator+'</option>';
+						
+				var countryOptionsHtml = '<option value="" selected hidden>Country</option>';
+				for (let country of countriesList)
+					countryOptionsHtml += '<option value="'+country+'">'+country+'</option>';
 
-		var pairHtml = 
-		'<div id="pairs">'+
-			'<div class="form-row mb-2">'+
-				'<div class="col">'+
-					'<select class="form-control" name="country">'+
-					countryOptionsHtml+
-					'</select>'+
-				 '</div>' +
-				'<div class="col">'+
-                	'<select class="form-control" name="indicator">'+
-                    	indicatorOptionsHtml+
-                    '</select>'+
-                 '</div>'+
-				'<div class="col-auto">' +
-					'<button type="button" class="btn btn-danger removePair">&times;</button>' +
-				'</div>' +
-			'</div>' +
-		'</div>';
+				var pairHtml = 
+				'<div id="pairs">'+
+					'<div class="form-row mb-2">'+
+						'<div class="col">'+
+							'<select class="form-control" name="country">'+
+							countryOptionsHtml+
+							'</select>'+
+						 '</div>' +
+						'<div class="col">'+
+		                	'<select class="form-control" name="indicator">'+
+		                    	indicatorOptionsHtml+
+		                    '</select>'+
+		                 '</div>'+
+						'<div class="col-auto">' +
+							'<button type="button" class="btn btn-danger removePair">&times;</button>' +
+						'</div>' +
+					'</div>' +
+				'</div>';
 
-		$('#pairs').append(pairHtml);
-	});
-
-	$(document).on('click', '.removePair', function() {
-		$(this).closest('.form-row').remove();
-	});
-
-	$('form').submit(function(event) {
-		event.preventDefault();
-
-		var chartType = $('#chartType').val();
-		var formData = {
-				  "chartType": chartType,
-				  "countries": [],
-				  "indicators": []
-				};
-		
-		$('#pairs .form-row').each(function() {
-			var country = $(this).find('select[name="country"]').val();
-		    var indicator = $(this).find('select[name="indicator"]').val();
-		    if (indicator && country){
-		    	formData.countries.push(country);
-		    	formData.indicators.push(indicator);
-		    }
-		});
-		
-		if (chartType == "scatter"){
-			if (formData.countries.length > 2 ){
-				alert("To generate a scatter plot, it is necessary to select two pairs of countries and indicators," +
-						" where the countries chosen for comparison are the same.");
-				return;
-			}
-			if (formData.countries[0] != formData.countries[1]){
-				alert("To generate a Scatter Chart the selected countries should be the same!");
-				return;
-			}
-			
-		}
-		
-		fetch('/generateChart', {
-			  method: 'POST',
-			  headers: {
-			    'Content-Type': 'application/json'
-			  },
-			  body: JSON.stringify(formData)
-			})
-			.then(response => {
-			  if (!response.ok) {
-			    throw new Error('Network response was not ok');
-			  }
-			  window.location.href = response.url; // Redirect to the new URL
-			})
-			.catch(error => {
-			  console.error('There was an error:', error);
+				$('#pairs').append(pairHtml);
 			});
-		
-		//alert('Chart type: ' + chartType + '\nPairs: ' + JSON.stringify(pairsList));
-		
-	});
-});
+
+			$(document).on('click', '.removePair', function() {
+				$(this).closest('.form-row').remove();
+			});
+
+			$('form').submit(function(event) {
+				event.preventDefault();
+
+				var chartType = $('#chartType').val();
+
+				var minYear = $('input[name="number1"]').val();
+				var maxYear = $('input[name="number2"]').val();
+				var aggregate = $('select[name="selectChoice"]').val();
+				
+				console.log(chartType);
+				console.log(minYear);
+				console.log(maxYear);
+				console.log(aggregate);
+
+				var formData = {
+						  "chartType": chartType,
+						  "countries": [],
+						  "indicators": [],
+						  "minYear": minYear,
+						  "maxYear": maxYear,
+						  "aggregate": aggregate,
+				};
+				
+				$('#pairs .form-row').each(function() {
+					var country = $(this).find('select[name="country"]').val();
+				    var indicator = $(this).find('select[name="indicator"]').val();
+				    if (indicator && country){
+				    	formData.countries.push(country);
+				    	formData.indicators.push(indicator);
+				    }
+				});
+				
+				if (chartType == "scatter"){
+					if (formData.countries.length > 2 ){
+						alert("To generate a scatter plot, it is necessary to select two pairs of countries and indicators," +
+								" where the countries chosen for comparison are the same.");
+						return;
+					}
+					if (formData.countries[0] != formData.countries[1]){
+						alert("To generate a Scatter Chart the selected countries should be the same!");
+						return;
+					}
+					
+				}
+				
+				fetch('/generateChart', {
+					  method: 'POST',
+					  headers: {
+					    'Content-Type': 'application/json'
+					  },
+					  body: JSON.stringify(formData)
+					})
+					.then(response => {
+					  if (!response.ok) {
+					    throw new Error('Network response was not ok');
+					  }
+					  window.location.href = response.url; // Redirect to the new URL
+					})
+					.catch(error => {
+					  console.error('There was an error:', error);
+					});
+				
+				//alert('Chart type: ' + chartType + '\nPairs: ' + JSON.stringify(pairsList));
+				
+			});
+		});
